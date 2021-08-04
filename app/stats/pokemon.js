868c71201adbd9966c99413cb7c8f9607f655a51
RobPanneton <robert.panneton@gmail.com>
2021-08-03 21:47:35 -0400

    transfer python to node

---
+++ app/stats/pokemon.js
@@ -0,0 +1,54 @@
+const pokemon = {
+  venusaur: {
+    id: 3,
+    type: { type1: "grass", type2: "poison" },
+    stats: {
+      hp: 80,
+      attack: 82,
+      defense: 83,
+      special: 100,
+      speed: 80,
+    },
+    attacks: ["tackle", "growl", "leech seed", "vine whip"],
+  },
+  ratticate: {
+    id: 20,
+    type: { type1: "normal", type2: None },
+    stats: {
+      hp: 55,
+      attack: 81,
+      defense: 60,
+      special: 50,
+      speed: 97,
+    },
+    attacks: [
+      "tackle",
+      "tail whip",
+      "quick attack",
+      "hyper fang",
+      "super fang",
+    ],
+  },
+  tauros: {
+    id: 128,
+    type: { type1: "normal", type2: None },
+    stats: {
+      hp: 75,
+      attack: 100,
+      defense: 95,
+      special: 70,
+      speed: 110,
+    },
+    attacks: [
+      "tackle",
+      "tail whip",
+      "stomp",
+      "leer",
+      "rage",
+      "take down",
+      "strength",
+    ],
+  },
+};
+
+module.exports = pokemon;
