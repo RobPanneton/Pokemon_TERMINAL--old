c3c929efe1bc38e181b0e569f8357137b997abb1
RobPanneton <robert.panneton@gmail.com>
2021-08-07 23:45:03 -0400

    add text art for intro screen

--- app/intro.js
+++ app/intro.js
@@ -1,11 +1,52 @@
 const { anyKeyPrompt } = require("./utils");
 
 const introScreen = async () => {
-  console.log("Pokemon TERMINAL");
+  console.log(
+    "                                 _                                 "
+  );
+  console.log(
+    "                                |/                                  "
+  );
+  console.log(
+    "                     _         |/                                  "
+  );
+  console.log(
+    "                    | |      ___                                   "
+  );
+  console.log(
+    "   ______           | | _  .° _ °.    _______                      "
+  );
+  console.log(
+    "   \\   _ °.   ___   | |/ / | |_//_  .° _   _ °.   ___              "
+  );
+  console.log(
+    "    | |_| | .° _ °. |   <  °._____| | / | | \\ | .° _ °.  ___       "
+  );
+  console.log(
+    "    |  __.° | |_| | |_|\\_\\     ___  |_| |_| |_| | |_| | |   °.     "
+  );
+  console.log(
+    "    | |     °.___.°          .°   °.            °.___.° | |°. |    "
+  );
+  console.log(
+    "    |_|                     /   _   \\                   |_| |_|    "
+  );
+  console.log(
+    "                           |---(_)---|                             "
+  );
+  console.log(
+    "                            \\       /                              "
+  );
+  console.log(
+    "                             °.___.°                               "
+  );
+  console.log("");
   await setTimeout(() => {
     // anyKeyPrompt("press any key to continue");
   }, 2000);
   anyKeyPrompt("press any key to continue");
 };
+
+console.log(introScreen());
 
 module.exports = { introScreen };
